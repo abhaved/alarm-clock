e0162445ac88537968bb5f14c6fd038946845503
bedmiston1996 <bedmiston@brooklynfriends.org>
2015-07-16 15:23:53 +0000

    controller changes

--- application_controller.rb
+++ application_controller.rb
@@ -1,12 +1,18 @@
 require 'bundler'
-require_relative 'models/alarm_clock.rb'
+require_relative 'models/riddle.rb'
+require_relative 'models/alarm.rb'
 Bundler.require
 
 class MyApp < Sinatra::Base
   
-  get '/' do  #/about_me == www.mywebsite.com/about_me / == www.mywebsite.com/
-
+  get '/' do
+    @time = user_time
     erb :index
+  end
+  
+  get '/video' do  #/about_me == www.mywebsite.com/about_me / == www.mywebsite.com/
+    @question = riddle
+    erb :video
   end  
   
   
