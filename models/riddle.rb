ce249491846bd47e735bf65c388d23251bc5e2c2
bedmiston1996 <bedmiston@brooklynfriends.org>
2015-07-16 15:23:14 +0000

    riddle changes

--- models/riddle.rb
+++ models/riddle.rb
@@ -4,20 +4,22 @@ def riddle
   prng = Random.new
   @rand_num=prng.rand(0...@riddles.length)
   @num = @rand_num
-  return @riddles[@num][0]
+  puts @riddles[@num][0]
 end
 
-def ask_riddle
-  puts riddle
-end
+
 
 def answer_method
-  unless @answer[:answer].include?(@riddles[@num][1])
+  until @answer.include?(@riddles[@num][1])
     puts "try again!"
-    ask_riddle
-  end
-  puts "correct!"
+    riddle
+    @answer = gets.chomp
+end
+    puts "correct!"
 end
 
 riddle
-ask_riddle
+
+@answer = gets.chomp
+
+answer_method
